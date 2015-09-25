4012a21339968022c47866fe1845ef357f9de77a
El Javiero <javigc@gmail.com>
2015-09-25 14:14:05 +0200

    Añadiendo acción

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
 include_once("config.php");
-
 $_GET['cache'] = 'no';
 
 /**
  ====== PROGRAMA PRINCIPAL ======
  */
+
+$diasDeLaSemana = array('Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado');
 
 $loader = new Twig_Loader_Filesystem(RUTA_BASE.'plantillas');
 
@@ -18,17 +19,24 @@ else
     $twig = new Twig_Environment($loader,array('charset'=>'utf-8',
                                                'cache'=>DIR_CACHE));
 if ($_POST['submit_dia']=='on')
-  $mensaje = 'EN DESARROLLO';
+  {
+  $timestamp = strtotime($_POST['anho'].'-'.$_POST['mes'].'-'.$_POST['dia']); 
+  $dia = getdate($timestamp);
+  }
+else
+  {
+  $dia = getdate();
+  }
 
-$html_centro = 
- "<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>";
+$diaDeLaSemana = $diasDeLaSemana[$dia['wday']];
+
 
 echo $twig->render('que-dia-fue.html.twig', 
                     array('analytics' => file_get_contents(RUTA_ABSOLUTA_WWW.'includes/analyticstracking.php'),
                           'mensaje' => $mensaje, 
                           'self_url' => URL_BASE,
-                          'hoy' => getdate(),
-                          'html_centro' => $html_centro)
+                          'dia' => $dia,
+                          'diaDeLaSemana' => $diaDeLaSemana)
                     );
 
 /** 
